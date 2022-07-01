74763195725934f05bb372bcd206cad567b82207
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-07-01 11:35:01 +0200

    milstone1 fatta

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -5,16 +5,18 @@ import java.time.format.DateTimeFormatter;
 
 public class Evento {
 private String titolo;
-private LocalDate dataPrenotazione;
-private LocalDate dataOggi = LocalDate.now();
-private int postiTotali = 400;
-private int postiPrenotati = 0 ;
+private LocalDate dataEveneto;
+private int postiTotali;
+private int postiPrenotati;
 private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-public Evento(String titolo, LocalDate dataOggi, int postiTotali) {
+public Evento(String titolo, LocalDate dataPrenotazione, int postiTotali)  throws Exception{
 	super();
 	this.titolo = titolo;
-	this.dataPrenotazione = dataPrenotazione;
+	this.dataEveneto = dataPrenotazione;
 	this.postiTotali = postiTotali;
+    controlloData();
+	controlloPosti();
+	
 }
 public String getTitolo() {
 	return titolo;
@@ -22,8 +24,8 @@ public String getTitolo() {
 public void setTitolo(String titolo) {
 	this.titolo = titolo;
 }
-public LocalDate getDataOggi() {
-	return dataOggi;
+public LocalDate getDataPrenotazione() {
+	return dataEveneto;
 }
 public int getPostiTotali() {
 	return postiTotali;
@@ -32,9 +34,46 @@ public int getPostiPrenotati() {
 	return postiPrenotati;
 }
 public void controlloData () throws Exception {
-	if(dataPrenotazione.isBefore(dataOggi)) {
+	if(dataEveneto.isBefore(LocalDate.now())) {
 	throw new Exception ("data non valida");
 	}   
+}
+private void controlloPosti () throws Exception {
+	if(postiTotali <= 0) {
+		throw new Exception ("Errore Posti prenotati uguale o inferiore a 0");
+	}
 
+	}
+
+/*prenota: aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha più
+posti disponibili deve sollevare un’eccezione*/
+
+public int prenota() throws Exception {
+	if(postiPrenotati == postiTotali ) {
+		throw new Exception ("capienza massima superata ");
+   	}
+	if (dataEveneto.isBefore(LocalDate.now())) {
+		throw new Exception ("data evento superata");
+	}
+	postiPrenotati +=1;
+	return postiPrenotati;	 
+	}
+/*disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono
+prenotazioni deve sollevare un’eccezione.*/
+public int disdici() throws Exception {
+	if (dataEveneto.isBefore(LocalDate.now())) {
+		throw new Exception ("data evento superata");
+	}
+	if (postiPrenotati <= 0) {
+		throw new Exception ("Nessuna prenotazione presente");
+	}
+	
+	return postiPrenotati=-1;	 
+	}
+/*l’override del metodo toString() in modo che venga restituita una stringa
+contenente: data formattata - titolo*/
+public String toString() {
+	return ("la data dell'evento è " + formatter.format(LocalDate.now()));
 }
 }
+
