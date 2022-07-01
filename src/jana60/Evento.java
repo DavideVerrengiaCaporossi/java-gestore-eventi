ad33112085e99162bf1152b9f967ebe67e815ecc
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-07-01 10:06:13 +0200

    first commit

---
+++ src/jana60/Evento.java
@@ -0,0 +1,39 @@
+package jana60;
+
+import java.time.LocalDate;
+
+
+public class Evento {
+private String titolo;
+private LocalDate dataPrenotazione;
+private LocalDate dataOggi = LocalDate.now();
+private int postiTotali = 400;
+private int postiPrenotati = 0 ;
+
+public Evento(String titolo, LocalDate dataOggi, int postiTotali) {
+	super();
+	this.titolo = titolo;
+	this.dataPrenotazione = dataPrenotazione;
+	this.postiTotali = postiTotali;
+}
+public String getTitolo() {
+	return titolo;
+}
+public void setTitolo(String titolo) {
+	this.titolo = titolo;
+}
+public LocalDate getDataOggi() {
+	return dataOggi;
+}
+public int getPostiTotali() {
+	return postiTotali;
+}
+public int getPostiPrenotati() {
+	return postiPrenotati;
+}
+public void controlloData () throws Exception {
+	if(dataPrenotazione.isBefore(dataOggi)) {
+	throw new Exception ("data non valida");
+	}
+}
+}
