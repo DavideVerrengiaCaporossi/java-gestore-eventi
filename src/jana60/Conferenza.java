50b7f353395186275f8d9faa981b4ed20bb9241e
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-07-01 18:02:53 +0200

    aggiornamento continuo milestone3

--- src/jana60/Conferenza.java
+++ src/jana60/Conferenza.java
@@ -4,25 +4,31 @@ import java.time.LocalDate;
 
 public class Conferenza extends Evento {
 	private String argomento ;
-	private String oratore;
-	public Conferenza(String titolo, LocalDate dataEveneto, int postiTotali, String argomento, String oratore)
+	private Oratore oratore;
+	
+public String getArgomento() {
+		return argomento;
+	}
+
+	public void setArgomento(String argomento) {
+		this.argomento = argomento;
+	}
+
+	public Oratore getOratore() {
+		return oratore;
+	}
+
+	public void setOratore(Oratore oratore) {
+		this.oratore = oratore;
+	}
+
+public Conferenza(String titolo, LocalDate dataEveneto, int postiTotali, String argomento, Oratore oratore)
 			throws Exception {
 		super(titolo, dataEveneto, postiTotali);
 		this.argomento = argomento;
 		this.oratore = oratore;
 	}
-	public String getArgomento() {
-		return argomento;
-	}
-	public void setArgomento(String argomento) {
-		this.argomento = argomento;
-	}
-	public String getOratore() {
-		return oratore;
-	}
-	public void setOratore(String oratore) {
-		this.oratore = oratore;
-	}
+
 @Override
 public String toString() {
 	return super.toString() + ("in questa conferenza parliamo di " + argomento + "ce n'è parlerà " + oratore) ;
---
+++ src/jana60/MainMilestone3.java
@@ -0,0 +1,90 @@
+package jana60;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class MainMilestone3 {
+
+	public static void main(String[] args) {
+		/*1. Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento
+		con tutti i parametri.
+		2. Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni
+		vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo
+		eventuali eccezioni.
+		3. Stampare a video il numero di posti prenotati e quelli disponibili
+		4. Chiedere all’utente se e quanti posti vuole disdire
+		5. Provare ad effettuare le disdette, implementando opportuni controlli e gestendo
+		eventuali eccezioni
+		6. Stampare a video il numero di posti prenotati e quelli disponibili*/
+		
+		Scanner scan = new Scanner(System.in) ;
+		
+		System.out.println("Benvenuto, inserisci il nome del evento");
+		
+		String titoloEvento = scan.nextLine();
+		System.out.println("inserisci data evento giorno/mese/anno");
+		
+		System.out.println("giorno: ");
+		int giorno = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("mese: ");
+		int mese = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("anno :");
+		int anno =Integer.parseInt(scan.nextLine());
+		
+		System.out.println(" inserisci Posti totali");
+		int capienzaEvento = Integer.parseInt(scan.nextLine());
+		
+		
+		
+		
+		//creo l'ogetto che contine data
+		LocalDate dataEvento = LocalDate.of(anno,mese,giorno);
+		
+		
+		try {
+			Evento ev1 = new Evento(titoloEvento,dataEvento,capienzaEvento);
+			
+			System.out.println("vuoi prenotare? si/no");
+			
+		String prenotazioniEvento = scan.nextLine();
+		
+		if(prenotazioniEvento.equalsIgnoreCase("si")) {
+			System.out.println("quante prenotazioni vuoi aggiungere?");
+			
+			int numeroPrenotazioniAggiuntive = Integer.parseInt(scan.nextLine());
+			
+			for (int i = 0; i < numeroPrenotazioniAggiuntive ; i++) {
+				ev1.prenota();
+				
+			}
+		}
+		System.out.println("la capienza totale del evento e di: " +capienzaEvento+ " il numero dei posti disponibili " + (capienzaEvento - ev1.getPostiPrenotati()) );
+		
+			System.out.println("vuoi disdire qualche prenotazioni ? si/no"); 
+			
+			String risposta = scan.nextLine();
+			
+			if(risposta.equalsIgnoreCase("si")) {
+				System.out.println("quante persone vuoi disdire?");
+				int numeroPersoneDisdette = Integer.parseInt(scan.nextLine());
+				for (int i = 0; i < numeroPersoneDisdette; i++) {
+					ev1.disdici();
+					
+				}
+			}
+			System.out.println("la capienza totale del evento e di: " +capienzaEvento+ " il numero dei posti disponibili " + (capienzaEvento - ev1.getPostiPrenotati()) );
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+				
+			
+			scan.close();	
+			
+	}
+
+}
